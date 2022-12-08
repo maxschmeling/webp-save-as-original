92b51779da95ae89d759acf1ac9bc5f493390635
Max Schmeling <max.schmeling@automattic.com>
2022-12-08 17:15:28 -0600

    Update to handle slow loading images

--- webp-save-as-original.js
+++ webp-save-as-original.js
@@ -1,38 +1,52 @@
-function canUseWebP() {
-	var elem = document.createElement('canvas');
-	if (!!(elem.getContext && elem.getContext('2d'))) {
-			return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
-	}
-	
-	return false;
-}
-
-function getPngDataUrl( img ) {
-  const canvas = document.createElement("canvas");
-
-  canvas.width = img.naturalWidth;
-  canvas.height = img.naturalHeight;
-
-  ctx = canvas.getContext("2d");
-  ctx.drawImage(img, 0, 0);
-  
-	if ( img.src.includes( '.jpg' ) || img.src.includes( '.jpeg' ) ) {
-  	return canvas.toDataURL("image/jpeg");
+( function () {
+	function canUseWebP() {
+		var elem = document.createElement('canvas');
+		if (!!(elem.getContext && elem.getContext('2d'))) {
+				return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+		}
+		
+		return false;
 	}
 
-	return canvas.toDataURL("image/png");
-}
+	function getConvertedDataUrl( img ) {
+		const canvas = document.createElement("canvas");
 
-try {
-	var images = document.querySelectorAll( 'img.webp-save-as-original' );
+		canvas.width = img.naturalWidth;
+		canvas.height = img.naturalHeight;
 
-	if ( !! images.length && canUseWebP() ) {
-		images.forEach( function ( img ) {
-			if ( img.src.includes( '.png' ) || img.src.includes( '.jpg' ) || img.src.includes( '.jpeg' ) ) {
-				img.src = getPngDataUrl( img );
+		ctx = canvas.getContext("2d");
+		ctx.drawImage(img, 0, 0);
+		
+		if ( img.src.includes( '.jpg' ) || img.src.includes( '.jpeg' ) ) {
+			return canvas.toDataURL("image/jpeg");
+		}
+
+		return canvas.toDataURL("image/png");
+	}
+
+	function swap( img ) {
+		if ( img.naturalWidth === 0 ) {
+			img.onload = function () {
+				if ( img.naturalWidth === 0 ) return;
+
+				img.src = getConvertedDataUrl( img );
 			}
-		} );
+		} else {
+			img.src = getConvertedDataUrl( img );
+		}
 	}
-} catch ( err ) {
-	console.error( err );
-}
+
+	try {
+		var images = document.querySelectorAll( 'img.webp-save-as-original' );
+
+		if ( !! images.length && canUseWebP() ) {
+			images.forEach( function ( img ) {
+				if ( img.src.includes( '.png' ) || img.src.includes( '.jpg' ) || img.src.includes( '.jpeg' ) ) {
+					swap( img );
+				}
+			} );
+		}
+	} catch ( err ) {
+		console.error( err );
+	}
+} )( );
